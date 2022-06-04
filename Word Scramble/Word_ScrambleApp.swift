c6697d57c454b10eef9704ed9b2df980eb435431
alisherbaigazin <alisherbaigazin@gmail.com>
2022-06-05 02:39:50 +0600

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Alisher Baigazin on 05.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
